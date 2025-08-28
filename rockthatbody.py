a73ef412af34e3b8e934b96798ac676261a73389
Maiara Fers <fersmaiara@gmail.com>
2025-08-27 22:12:38 -0300

    Update rockthatbody.py

--- rockthatbody.py
+++ rockthatbody.py
@@ -5,20 +5,20 @@ from time import sleep
 def printLyrics():
     lines = [
         ("I wanna da-", 0.06), 
-        ("I wanna dance in the lights ✨", 0.05),
+        ("I wanna dance in the lights", 0.05),
         ("I wanna ro- ", 0.07),
-        ("I wanna rock your body ✨", 0.08),
+        ("I wanna rock your body", 0.08),
         ("I wanna go", 0.07),    
         ("I wanna go for a ride", 0.08),
         ("Hop in the music and", 0.068),
-        ("Rock your body ✨", 0.07),
+        ("Rock your body", 0.07),
         ("Rock that body", 0.08),
         ("Come on, come on", 0.069),
         ("Rock that body", 0.035),
-        ("(Rock your body) ✨", 0.05),
+        ("(Rock your body)", 0.05),
         ("Rock that body", 0.03),
         ("come on, come on", 0.049),
-        ("Rock that body ✨", 0.035),
+        ("Rock that body", 0.035),
         
     ]
 
